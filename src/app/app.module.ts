ec0097385a4cba2ecee494025ed118d2cd94eb07
Müller László <muller.laszlo@virgo.hu>
2020-04-01 21:49:42 +0200

    line + bar chart (de még kicsit randa)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { AppComponent } from './app.component';
 
 import { ChartsModule } from 'ng2-charts';
 import { ConstantsService } from './services/constants.service';
-// import { LineChartComponent } from './exchange/line-chart.component';
+import { ExchangesChartComponent } from './components/exchanges-chart/exchanges-chart.component';
+import { ExchangeChartComponent } from './components/exchange-chart/exchange-chart.component';
 import { SalesChartComponent } from './components/sales-chart/sales-chart.component';
 import { VolumesChartComponent } from './components/volumes-chart/volumes-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ExchangesChartComponent,
+    ExchangeChartComponent,
     SalesChartComponent,
     VolumesChartComponent
   ],
--- src/app/services/constants.service.ts
+++ src/app/services/constants.service.ts
@@ -8,6 +8,7 @@ import * as data from '../proba_6.json';
 export class ConstantsService {
 
   readonly jsonData: any = (data as any).default;
+  readonly exchangeData = this.jsonData.apple_prices;
   readonly salesData = this.jsonData.sales;
   readonly volumesData = this.jsonData.volumes;
 
